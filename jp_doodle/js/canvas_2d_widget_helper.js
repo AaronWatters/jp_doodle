0c1f902ca6d63f43e6f98853ec8617b30f2bd9ff
Aaron Watters <awatters@simonsfoundation.org>
2018-07-11 12:21:52 -0400

    collect statistics on bounding box and support stroke/no fill

--- jp_doodle/js/canvas_2d_widget_helper.js
+++ jp_doodle/js/canvas_2d_widget_helper.js
@@ -36,13 +36,12 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
             target.canvas_context = target.canvas[0].getContext("2d");
             var ctx = target.canvas_context;
             var ts = target.canvas_translate_scale;
-            ctx.translate(ts.x, ts.y);
-            ctx.scale(ts.w, ts.h);
+            //ctx.translate(ts.x, ts.y);
+            //ctx.scale(ts.w, ts.h);
             ts.model_height = h * ts.h;
             ts.model_intercept = 2 * ts.y + ts.model_height;
+            target.clear_canvas();
         };
-
-        target.reset_canvas();
 
         target.clear_canvas = function () {
             // https://stackoverflow.com/questions/2142535/how-to-clear-the-canvas-for-redrawing
@@ -53,7 +52,12 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
             context.clearRect(0, 0, canvas.width, canvas.height)
             context.translate(ts.x, ts.y);
             context.scale(ts.w, ts.h);
+            if (target.canvas_stats) {
+                target.canvas_stats = {};
+            }
         };
+
+        target.reset_canvas();
 
         // Some functions useful for Jupyter/proxy interface:
         target.canvas_call = function(method_name, args) {
@@ -61,6 +65,32 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
             var method = context[method_name];
             method.apply(context, context, args);
         };
+
+        // Set to mapping to collect stats
+        target.canvas_stats = null;
+
+        target.add_point_stats = function (x, y) {
+            var stats = target.canvas_stats;
+            if (stats) {
+                if (stats.count) {
+                    // add an additional point
+                    stats.count += 1;
+                    stats.min_x = Math.min(x, stats.min_x);
+                    stats.max_x = Math.max(x, stats.max_x);
+                    stats.min_y = Math.min(y, stats.min_y);
+                    stats.max_y = Math.max(y, stats.max_y);
+                } else {
+                    // add the first point
+                    stats.count = 1;
+                    stats.min_x = x;
+                    stats.max_x = x;
+                    stats.min_y = y;
+                    stats.max_y = y;
+                }
+            } else {
+                return;   // do nothing -- could be an error?
+            }
+        }
 
         target.canvas_assign = function(slot_name, value) {
             target.canvas_context[slot_name] = value;
@@ -71,13 +101,19 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
                 color: target.canvas_fillColor,
                 start: 0,
                 arc: 2 * Math.PI,
+                fill: true,  // if false then do a outline
             }, opt);
             var context = target.canvas_context;
             context.beginPath();
-            context.fillStyle = s.color;
+            //context.fillStyle = s.color;
             var center = target.converted_location(s.x, s.y);
             context.arc(center.x, center.y, s.r, s.start, s.arc);
-            context.fill();
+            fill_or_stroke(context, s);
+            // update stats
+            if (target.canvas_stats) {
+                target.add_point_stats(s.x + s.r, s.y + s.r);
+                target.add_point_stats(s.x - s.r, s.y - s.r);
+            }
             return s;
         };
 
@@ -95,6 +131,11 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
             context.moveTo(p1.x, p1.y);
             context.lineTo(p2.x, p2.y);
             context.stroke();
+            // update stats
+            if (target.canvas_stats) {
+                target.add_point_stats(s.x1, s.y1);
+                target.add_point_stats(s.x2, s.y2);
+            }
             return s;
         };
 
@@ -111,8 +152,32 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
             // XXX text align?
             context.fillText(s.text, 0, 0); // translated to (x,y)
             context.restore();  // matches translate_and_rotate
+            // update stats
+            if (target.canvas_stats) {
+                var width = context.measureText(s.text).width;
+                var height = width * 1.4 / s.text.length;  // fudge...
+                target.rectangle_stats(s.x, s.y, width, height, s.degrees);
+            }
             return s;
         };
+
+        target.rectangle_stats = function(x, y, w, h, degrees) {
+            var radians = 0.0;
+            if (degrees) {
+                radians = degrees * Math.PI / 180.0;
+            }
+            var c = Math.cos(radians);
+            var s = Math.sin(radians);
+            var add_offset = function (dx, dy) {
+                var x1 = x + (dx * c - dy * s);
+                var y1 = y + (dx * s + dy * c);
+                target.add_point_stats(x1, y1);
+            };
+            target.add_point_stats(x, y);
+            add_offset(w, 0);
+            add_offset(0, h);
+            add_offset(w, h);
+        }
 
         target.translate_and_rotate = function(x, y, degrees) {
             var context = target.canvas_context;
@@ -130,26 +195,44 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
         target.rect = function(opt) {
             var s = $.extend({
                 color: target.canvas_fillColor,
+                fill: true,  // if false then do a outline
             }, opt);
             target.translate_and_rotate(s.x, s.y, s.degrees);
             var context = target.canvas_context;
             context.beginPath();
-            context.fillStyle = s.color;
+            //context.fillStyle = s.color;
             var height = s.h;
             if (target.canvas_y_up) {
                 height = -height;
             }
             context.rect(0, 0, s.w, height)  // translated to (x,y)
-            context.fill();
+            fill_or_stroke(context, s);
             context.restore();  // matches translate_and_rotate
+            // update stats
+            if (target.canvas_stats) {
+                target.rectangle_stats(s.x, s.y, s.w, s.h, s.degrees);
+            }
+            return s;
         }
+
+        var fill_or_stroke = function(context, s) {
+            if (s.fill) {
+                context.fillStyle = s.color;
+                context.fill();
+            } else {
+                context.strokeStyle = s.color;
+                context.stroke();
+            }
+        };
 
         target.polygon = function(opt) {
             var s = $.extend({
                 color: target.canvas_fillColor,
+                fill: true,  // if false then do a outline
+                close: true,
             }, opt);
             var context = target.canvas_context;
-            context.fillStyle = s.color;
+            //context.fillStyle = s.color;
             var points = s.points;
             context.beginPath();
             var point0 = points[0];
@@ -160,8 +243,18 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
                 var pc = target.converted_location(point[0], point[1]);
                 context.lineTo(pc.x, pc.y);
             }
-            context.closePath();
-            context.fill();
+            if (s.close) {
+                context.closePath();
+            }
+            fill_or_stroke(context, s);
+            // update stats
+            if (target.canvas_stats) {
+                for (var i=0; i<points.length; i++) {
+                    var point = points[i];
+                    target.add_point_stats(point[0], point[1]);
+                } 
+            }
+            return s;
         };
 
         target.color_at = function(pixel_x, pixel_y) {
@@ -233,7 +326,6 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
     };
 
     $.fn.canvas_2d_widget_helper.example = function(element) {
-        debugger;
         element.empty();
         element.css("background-color", "cornsilk").width("520px");
         var config = {
@@ -242,19 +334,31 @@ Structure follows: https://learn.jquery.com/plugins/basic-plugin-creation/
             translate_scale: {x: 20, y:30, w:0.8, h:0.7},
         }
         element.canvas_2d_widget_helper(element, config);
+        element.canvas_stats = {};
+        var show_stats = function (color) {
+            var stats = element.canvas_stats;
+            var width = stats.max_x - stats.min_x;
+            var height = stats.max_y - stats.min_y;
+            element.rect({x: stats.min_x, y: stats.min_y, w: width, h:height, color:color, fill: false})
+            element.canvas_stats = {};
+        }
         element.circle({name: "green circle", x:160, y:70, r:20, color:"green"});
+        show_stats("red");
         element.rect({name:"a rect", x:10, y:50, w:10, h:120, color:"salmon", degrees:-15});
+        show_stats("green");
         element.text({name:"some text", x:40, y:40, text:"Canvas", color:"#f4d", degrees:45});
+        show_stats("blue");
         element.line({name:"a line", x1:100, y1:100, x2:150, y2:130, color:"brown"});
+        show_stats("black");
         element.polygon({
             name: "polly", 
             points: [[250,100], [400,100], [280,180], [375,60], [370,180]],
             color: "#ffd",
         });
+        show_stats("white");
         var c = element.color_at(160, 70);
         var info = $("<div>a canvas.  color at 160 70: " + c.data + "</div>").appendTo(element);
         element.canvas.mousemove(function (e) {
-            debugger;
             var color = element.event_color(e);
             var ploc = element.event_pixel_location(e);
             var cloc = element.event_canvas_location(e);
