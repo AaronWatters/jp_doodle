d4547dd7e0c4b4024be99ce7f58fbb8da629e2b9
Aaron Watters <awatters@simonsfoundation.org>
2018-08-10 12:43:44 -0400

    add frame wrappers that depend on unreleased jp_proxy_widget features

--- jp_doodle/dual_canvas.py
+++ jp_doodle/dual_canvas.py
@@ -32,6 +32,8 @@ class CanvasOperationsMixin(object):
     Mixin for shared operations for different forms of canvas frames.
     """
 
+    # xxxx Not all methods may make sense for all subclasses.
+
     def circle(self, x, y, radius, color, fill=True, **other_args):
         "Draw a circle or arc on the canvas frame."
         s = dict(x=x, y=y, r=radius, color=color, fill=fill)
@@ -65,29 +67,6 @@ class CanvasOperationsMixin(object):
         s = dict(points=points, color=color, close=close, fill=fill)
         s.update(other_args)
         self.call_method("polygon", s)
-
-
-class DualCanvasWidget(jp_proxy_widget.JSProxyWidget, CanvasOperationsMixin):
-    
-    "Wrapper for dual_canvas jQuery extension object."
-
-    default_config = dict(width=400, height=400)
-    
-    def __init__(self, width=None, height=None, config=None, *pargs, **kwargs):
-        "Create a canvas drawing area widget."
-        super(DualCanvasWidget, self).__init__(*pargs, **kwargs)
-        load_requirements(self)
-        if config is None:
-            config = self.default_config.copy()
-        if height is not None:
-            config["height"] = height
-        if width is not None:
-            config["width"] = width
-        # Standard initialization
-        self.js_init("""
-            element.empty();
-            element.dual_canvas_helper(config);
-            """, config=config)
 
     def reset_canvas(self):
         "Re-initialize the canvas drawing area."
@@ -125,5 +104,83 @@ class DualCanvasWidget(jp_proxy_widget.JSProxyWidget, CanvasOperationsMixin):
         "For testing.  Deliver the color at pixel as a list of four integers to the callback(list_of_integers)."
         self.element.callback_with_pixel_color(pixel_x, pixel_y, callback)
 
+    def do_lasso(self, lasso_callback, delete_after=True, **config):
+        "Use a polygon to select named elements.  Return name --> description mappint to the callback."
+        self.element.do_lasso(lasso_callback, config, delete_after)
 
+    def named_vector_frame(self, name, x_vector, y_vector, xy_offset):
+        """
+        Attach a named vector frame to the widget element and return an interface for accessing it.
+        The vectors must be given as dictionaries like so: {"x": x_value, "y": y_value}.
+        """
+        # xxxx this doesn't make sense as a frame method?
+        self.js_init("""
+        // Attach the frame by name to the element
+        element[name] = element.vector_frame(x_vector, y_vector, xy_offset);
+        """, name=name, x_vector=x_vector, y_vector=y_vector, xy_offset=xy_offset)
+        # return an interface wrapper for the named frame
+        return FrameInterface(self, name)
+
+    def named_rframe(self, name, scale_x, scale_y, translate_x=0, translate_y=0):
+        """
+        Attach a named rectangular frame to the widget element and return an interface for accessing it.
+        """
+        # xxxx this doesn't make sense as a frame method?
+        self.js_init("""
+        // Attach the frame by name to the element
+        element[name] = element.rframe(scale_x, scale_y, translate_x, translate_y);
+        """, name=name, scale_x=scale_x, scale_y=scale_y, translate_x=translate_x, translate_y=translate_y)
+        # return an interface wrapper for the named frame
+        return FrameInterface(self, name)
+
+    def named_frame_region(self, name, minx, miny, maxx, maxy, frame_minx, frame_miny, frame_maxx, frame_maxy):
+        """
+        Attach a named frame region to the widget element and return an interface for accessing it.
+        """
+        # xxxx this doesn't make sense as a frame method?
+        self.js_init("""
+        // Attach the frame by name to the element
+        element[name] = element.frame_region(minx, miny, maxx, maxy, frame_minx, frame_miny, frame_maxx, frame_maxy);
+        """, name=name, minx=minx, miny=miny, maxx=maxx, maxy=maxy,
+        frame_minx=frame_minx, frame_miny=frame_miny, frame_maxx=frame_maxx, frame_maxy=frame_maxy)
+        # return an interface wrapper for the named frame
+        return FrameInterface(self, name)
+
+    def lower_left_axes(self, min_x, min_y, max_x, max_y, **other_args):
+        s = dict(min_x=min_x, min_y=min_y, max_x=max_x, max_y=max_y)
+        s.update(other_args)
+        #self.call_method("lower_left_axes", s)
+        self.element.lower_left_axes(s)
+
+class DualCanvasWidget(jp_proxy_widget.JSProxyWidget, CanvasOperationsMixin):
+    
+    "Wrapper for dual_canvas jQuery extension object."
+
+    default_config = dict(width=400, height=400)
+    
+    def __init__(self, width=None, height=None, config=None, *pargs, **kwargs):
+        "Create a canvas drawing area widget."
+        super(DualCanvasWidget, self).__init__(*pargs, **kwargs)
+        load_requirements(self)
+        if config is None:
+            config = self.default_config.copy()
+        if height is not None:
+            config["height"] = height
+        if width is not None:
+            config["width"] = width
+        # Standard initialization
+        self.js_init("""
+            element.empty();
+            element.dual_canvas_helper(config);
+            """, config=config)
+
+
+class FrameInterface(CanvasOperationsMixin):
+
+    "Wrapper for frame inside a dual_canvas which is attached to the widget element by name."
+
+    def __init__(self, from_widget, attribute_name):
+        self.from_widget = from_widget
+        self.attribute_name = attribute_name
+        self.element = self.from_widget.element[self.attribute_name]
 
