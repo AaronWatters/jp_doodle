c116dfc0252456a19734928cb7ea4c31b07211e0
Aaron Watters <awatters@simonsfoundation.org>
2018-11-15 10:04:40 -0500

    add stupid mock canvas

--- jest/globals.js
+++ jest/globals.js
@@ -6,4 +6,53 @@ if (!global.jQuery) {
 }
 
 require('jquery-ui');
+require('jest-canvas-mock');
   
+// https://github.com/jsdom/jsdom/issues/1782
+
+//
+// Mock Canvas / Context2D calls
+//
+function mockCanvas (window) {
+    window.HTMLCanvasElement.prototype.getContext = function () {
+        return {
+            fillRect: function() {},
+            clearRect: function(){},
+            resetTransform: function(){},
+            getImageData: function(x, y, w, h) {
+                return  {
+                    data: new Array(w*h*4)
+                };
+            },
+            putImageData: function() {},
+            createImageData: function(){ return []},
+            setTransform: function(){},
+            drawImage: function(){},
+            save: function(){},
+            fillText: function(){},
+            restore: function(){},
+            beginPath: function(){},
+            moveTo: function(){},
+            lineTo: function(){},
+            closePath: function(){},
+            stroke: function(){},
+            translate: function(){},
+            scale: function(){},
+            rotate: function(){},
+            arc: function(){},
+            fill: function(){},
+            measureText: function(){
+                return { width: 0 };
+            },
+            transform: function(){},
+            rect: function(){},
+            clip: function(){},
+        };
+    }
+
+    window.HTMLCanvasElement.prototype.toDataURL = function () {
+        return "";
+    }
+}
+
+global.mockCanvas = mockCanvas;
