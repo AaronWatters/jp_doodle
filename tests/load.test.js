cb09e99d4b50e32b243b19d5b1cebb8356f40b56
Aaron Watters <awatters@simonsfoundation.org>
2018-11-15 10:10:03 -0500

    tests using mocked canvas

--- tests/load.test.js
+++ tests/load.test.js
@@ -8,11 +8,32 @@ global.jQuery = function(argument) {
 // var index = require('../dist/index');
 import jp_doodle_is_loaded from "../dist/index";
 
-describe('testing my_plugin', () => {
+describe('testing loading jp_doodle plugins', () => {
 
     it('loads the index', () => {
         //expect(true).toEqual(true);
         expect(jp_doodle_is_loaded()).toBe(true);
+    });
+
+    it("defines the dual_canvas_helper plugin", () => {
+        expect(global.jQuery.fn.dual_canvas_helper).toBeTruthy();
+    });
+
+    it("defines the canvas_2d_widget_helper plugin", () => {
+        expect(global.jQuery.fn.canvas_2d_widget_helper).toBeTruthy();
+    });
+
+    it("creates a dual canvas", () => {
+        mockCanvas(window);
+        var elt = jQuery("<b>test</b>");
+        elt.dual_canvas_helper();
+        expect(elt.visible_canvas).toBeTruthy();
+    });
+
+    it("runs the dual canvas example", () => {
+        var elt = jQuery("<b>test</b>");
+        elt.dual_canvas_helper.example(elt);
+        expect(elt.visible_canvas).toBeTruthy();
     });
 
   });
